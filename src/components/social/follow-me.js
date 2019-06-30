d1c6f470f3e6bdd5c7f9f3c6e143dc3aeedeb5fd
Greg Parr <greg.parr@blueyonder.co.uk>
2019-06-30 21:32:51 +0100

    Added some social contact info

---
+++ src/components/social/follow-me.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Follow } from 'react-twitter-widgets'
+
+const FollowMe = () => {
+    return (
+        <Follow username="gregparr" />
+    )
+}
+
+export default FollowMe
